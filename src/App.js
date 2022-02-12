480b925d4d9eb6a5aa487368f0651003b7ee2a60
Esoolgnah <nezcoreen@gmail.com>
2022-02-12 12:17:20 +0900

    2강 완료

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
 import React, { useState } from 'react';
-import { Navbar,Container,Nav,NavDropdown } from  'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
+import Data from './data.js';
+import 상품 from './상품.js';
 
 function App() {
-
-  let [shoes, shoes변경] = useState([])
-  let [가격, 가격변경] = useState(0)
+  let [shoes, shoes변경] = useState(Data);
   return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
         <Container>
-           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#link'>Link</Nav.Link>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Separated link
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <div className='Jumbotron'>
-        <div className ='JumbotronBackground'>
-          <h1 className ='JumbotronHello'>20% Season Off</h1>
-          <p className = 'JumbotronWelcome'>Welcome to ShoeShop!</p>
-          <button className = 'JumbotronButton'>Shop</button>
+        <div className='JumbotronBackground'>
+          <h1 className='JumbotronHello'>20% Season Off</h1>
+          <p className='JumbotronWelcome'>Welcome to ShoeShop!</p>
+          <button className='JumbotronButton'>Shop</button>
         </div>
       </div>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src = 'https://codingapple1.github.io/shop/shoes1.jpg' width='100%'/>
-            <h4>상품</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className='col-md-4'>
-          <img src = 'https://codingapple1.github.io/shop/shoes2.jpg' width='100%'/>
-            <h4>상품</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className='col-md-4'>
-          <img src = 'https://codingapple1.github.io/shop/shoes3.jpg' width='100%'/>
-            <h4>상품</h4>
-            <p>상품설명 & 가격</p>
-          </div>
+          {shoes.map(item => {
+            return (
+              <상품
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                content={item.content}
+                price={item.price}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,20 +1,23 @@
-export default[
+export default [
   {
-    id : 0,
-    title : "White and Black",
-    content : "Born in France",
-    price : 120000
+    id: 0,
+    image: 'https://codingapple1.github.io/shop/shoes1.jpg',
+    title: 'White and Black',
+    content: 'Born in France',
+    price: 120000,
   },
   {
-    id : 1,
-    title : "Red Knit",
-    content : "Born in Seoul",
-    price : 110000
+    id: 1,
+    image: 'https://codingapple1.github.io/shop/shoes2.jpg',
+    title: 'Red Knit',
+    content: 'Born in Seoul',
+    price: 110000,
   },
   {
-    id : 2,
-    title : "Grey Yordan",
-    content : "Born in the States",
-    price : 130000
-  }
-] 
+    id: 2,
+    image: 'https://codingapple1.github.io/shop/shoes3.jpg',
+    title: 'Grey Yordan',
+    content: 'Born in the States',
+    price: 130000,
+  },
+];
