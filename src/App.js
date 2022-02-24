289a797e6f1088f3137e57c0cb54906eb06057ca
Esoolgnah <nezcoreen@gmail.com>
2022-02-24 23:55:11 +0900

    4강 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
@@ -32,20 +33,40 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className='Jumbotron'>
-        <div className='JumbotronBackground'>
-          <h1 className='JumbotronHello'>20% Season Off</h1>
-          <p className='JumbotronWelcome'>Welcome to ShoeShop!</p>
-          <button className='JumbotronButton'>Shop</button>
+      <Route exact path='/'>
+        <div className='Jumbotron'>
+          <div className='JumbotronBackground'>
+            <h1 className='JumbotronHello'>20% Season Off</h1>
+            <p className='JumbotronWelcome'>Welcome to ShoeShop!</p>
+            <button className='JumbotronButton'>Shop</button>
+          </div>
         </div>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          {shoes.map((a, i) => {
-            return <Card shoes={a} i={i} key={i} />;
-          })}
+        <div className='container'>
+          <div className='row'>
+            {shoes.map((a, i) => {
+              return <Card shoes={a} i={i} key={i} />;
+            })}
+          </div>
         </div>
-      </div>
+      </Route>
+      <Route path='/detail'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <img
+                src='https://codingapple1.github.io/shop/shoes1.jpg'
+                width='100%'
+              />
+            </div>
+            <div className='col-md-6 mt-4'>
+              <h4 className='pt-5'>상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className='btn btn-danger'>주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
     </div>
   );
 }
