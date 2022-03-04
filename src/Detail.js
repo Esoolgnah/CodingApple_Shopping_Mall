0fef5012028c9f5d5af18a996f3b19b6f44b210c
Esoolgnah <nezcoreen@gmail.com>
2022-03-04 20:03:04 +0900

    16강 완료

--- src/Detail.js
+++ src/Detail.js
@@ -1,8 +1,10 @@
+import { Nav } from 'react-bootstrap';
 import React, { useEffect, useState, useContext } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
 import { 재고context } from './App.js';
+import { CSSTransition } from 'react-transition-group';
 
 let 박스 = styled.div`
   padding: 20px;
@@ -13,11 +15,15 @@ let 제목 = styled.h4`
 `;
 
 function Detail(props) {
+  let history = useHistory();
   let { id } = useParams();
   let 찾은상품 = props.shoes.find(function (상품) {
     return 상품.id == id;
   });
   const [alert, alert변경] = useState(true);
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경] = useState(false);
+
   let 재고 = useContext(재고context);
 
   useEffect(() => {
@@ -54,9 +60,7 @@ function Detail(props) {
           <h4 className='pt-5'>{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
-
           <Info 재고={props.재고} />
-
           <button
             className='btn btn-danger'
             onClick={() => {
@@ -65,10 +69,56 @@ function Detail(props) {
           >
             주문하기
           </button>
+          &nbsp;
+          <button
+            className='btn btn-danger'
+            onClick={() => {
+              history.push('/');
+            }}
+          >
+            뒤로가기
+          </button>
+          <Nav className='mt-5' variant='tabs' defaultActiveKey='/home'>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='link-0'
+                onClick={() => {
+                  스위치변경(false);
+                  누른탭변경(0);
+                }}
+              >
+                Active
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey='link-1'
+                onClick={() => {
+                  스위치변경(false);
+                  누른탭변경(1);
+                }}
+              >
+                Option 2
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <CSSTransition in={스위치} classNames='wow' timeout={500}>
+            <TabContent 누른탭={누른탭} 스위치변경={스위치변경} />
+          </CSSTransition>
         </div>
       </div>
     </div>
   );
+}
+
+function TabContent(props) {
+  useEffect(() => {
+    props.스위치변경(true);
+  });
+
+  if (props.누른탭 === 0) return <div>0번째 내용</div>;
+  else if (props.누른탭 === 1) return <div>1번째 내용</div>;
+  else if (props.누른탭 === 2) return <div>2번째 내용</div>;
 }
 
 function Info(props) {
