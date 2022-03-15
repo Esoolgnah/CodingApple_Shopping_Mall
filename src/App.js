bc3937ae84e32d3f711f6fc87437af3c0719534d
Esoolgnah <nezcoreen@gmail.com>
2022-03-15 21:53:49 +0900

    31강 완료(완)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React, { useState, useContext, lazy, Suspense } from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import axios from 'axios';
 import Data from './data.js';
@@ -13,6 +13,7 @@ import { Link, Route, Switch, useHistory } from 'react-router-dom';
 export let 재고context = React.createContext();
 
 function App() {
+  let history = useHistory();
   let [shoes, shoes변경] = useState(Data);
   let [더보기, 더보기변경] = useState(false);
   let [재고, 재고변경] = useState([10, 11, 12]);
@@ -21,7 +22,9 @@ function App() {
     <div className='App'>
       <Navbar bg='light' expand='lg'>
         <Container>
-          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'>
+            ShoeShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
@@ -31,19 +34,6 @@ function App() {
               <Nav.Link as={Link} to='/cart'>
                 Cart
               </Nav.Link>
-              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.2'>
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.3'>
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href='#action/3.4'>
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
